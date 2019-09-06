5d5c64d28f68f9051a18effc1c084fe7f0f3d693
Ninja <genreshinobi@gmail.com>
2019-09-05 21:41:07 -0600

    Added Creative Tab; Passing Iron Door as the icon

--- src/main/java/com/gs/supernaturals/Supernaturals.java
+++ src/main/java/com/gs/supernaturals/Supernaturals.java
@@ -1,8 +1,13 @@
 package com.gs.supernaturals;
 
+import com.gs.supernaturals.core.ModBlocks;
 import com.gs.supernaturals.setup.ClientProxy;
 import com.gs.supernaturals.setup.IProxy;
 import com.gs.supernaturals.setup.ServerProxy;
+import com.gs.supernaturals.util.Reference;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -17,6 +22,16 @@ public class Supernaturals {
 
     // Setup proxy
     public static IProxy proxy = DistExecutor.runForDist(() -> ClientProxy::new, () -> ServerProxy::new);
+
+    // Add Creative Tab
+    public static final ItemGroup creativeTab = new ItemGroup(Reference.MODID) {
+        // Sets the tab icon to the passed item
+        // TODO: Update the item passed to Supernaturals item
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(Blocks.IRON_DOOR);
+        }
+    };
 
     // Main
     public Supernaturals() {
