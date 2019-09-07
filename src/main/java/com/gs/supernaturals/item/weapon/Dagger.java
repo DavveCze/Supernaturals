b1b4c85b109bc0f27babc149447c3f730186df5b
Ninja <genreshinobi@gmail.com>
2019-09-07 00:08:43 -0600

    Added Item Support; Added Dagger

---
+++ src/main/java/com/gs/supernaturals/item/weapon/Dagger.java
@@ -0,0 +1,11 @@
+package com.gs.supernaturals.item.weapon;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.SwordItem;
+
+public class Dagger extends SwordItem {
+
+    public Dagger(IItemTier tier, int damageIn, float speedIn, SwordItem.Properties properties) {
+        super( tier, damageIn, speedIn, properties);
+    }
+}
