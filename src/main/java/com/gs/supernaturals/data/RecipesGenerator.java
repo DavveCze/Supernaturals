c5836eb37df0644dc1753495b728e01fd016c60f
lukas2005 <lmarianski@protonmail.com>
2020-04-14 23:54:06 +0200

    Added generated recipes

--- src/main/java/com/gs/supernaturals/data/RecipesGenerator.java
+++ src/main/java/com/gs/supernaturals/data/RecipesGenerator.java
@@ -1,11 +1,24 @@
 package com.gs.supernaturals.data;
 
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.IFinishedRecipe;
-import net.minecraft.data.RecipeProvider;
+import com.gs.supernaturals.blocks.ModBlocks;
+import com.gs.supernaturals.item.ModItemTier;
+import com.gs.supernaturals.item.ModItems;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.advancements.criterion.InventoryChangeTrigger;
+import net.minecraft.advancements.criterion.ItemPredicate;
+import net.minecraft.advancements.criterion.MinMaxBounds;
+import net.minecraft.block.Block;
+import net.minecraft.data.*;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemTier;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
 
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import static com.gs.supernaturals.Supernaturals.MOD_ID;
 
@@ -21,23 +34,50 @@ public class RecipesGenerator extends RecipeProvider {
 //                .addCriterion("has_silver_ore", InventoryChangeTrigger.Instance.forItems(ModBlocks.SILVER_ORE.get()))
 //                .build(consumer);
 //
-//        ShapelessRecipeBuilder.shapelessRecipe(ModBlocks.SILVER_BLOCK.get())
-//                .addIngredient(ModItems.SILVER_INGOT.get(), 9)
-//                .addCriterion("has_silver_ingot", InventoryChangeTrigger.Instance.forItems(ModItems.SILVER_INGOT.get()))
-//                .build(consumer);
-//        ShapelessRecipeBuilder.shapelessRecipe(ModItems.SILVER_INGOT.get(), 9)
-//                .addIngredient(ModBlocks.SILVER_BLOCK.get())
-//                .addCriterion("has_silver_ingot", InventoryChangeTrigger.Instance.forItems(ModItems.SILVER_INGOT.get()))
-//                .build(consumer, id("silver_ingot_from_block"));
-//
-//        ShapelessRecipeBuilder.shapelessRecipe(ModItems.SILVER_NUGGET.get(), 9)
-//                .addIngredient(ModItems.SILVER_INGOT.get())
-//                .addCriterion("has_silver_ingot", InventoryChangeTrigger.Instance.forItems(ModItems.SILVER_INGOT.get()))
-//                .build(consumer);
-//        ShapelessRecipeBuilder.shapelessRecipe(ModItems.SILVER_INGOT.get())
-//                .addIngredient(ModItems.SILVER_NUGGET.get(), 9)
-//                .addCriterion("has_silver_ingot", InventoryChangeTrigger.Instance.forItems(ModItems.SILVER_INGOT.get()))
-//                .build(consumer, id("silver_ingot_from_nugget"));
+
+        buildMaterialRecipeSet("silver"    , consumer, ModItems.SILVER_INGOT    , ModItems.SILVER_NUGGET    , ModBlocks.SILVER_BLOCK);
+        buildMaterialRecipeSet("white_gold", consumer, ModItems.WHITE_GOLD_INGOT, ModItems.WHITE_GOLD_NUGGET, ModBlocks.WHITE_GOLD_BLOCK);
+
+        buildDaggerRecipeForTier(ModItems.DAGGER           , ItemTier.IRON         , consumer);
+        buildDaggerRecipeForTier(ModItems.SILVER_DAGGER    , ModItemTier.SILVER    , consumer);
+        buildDaggerRecipeForTier(ModItems.WHITE_GOLD_DAGGER, ModItemTier.WHITE_GOLD, consumer);
+    }
+
+    public void buildDaggerRecipeForTier(Supplier<? extends Item> result, IItemTier tier, Consumer<IFinishedRecipe> consumer) {
+        Ingredient materialIng = tier.getRepairMaterial();
+        Item material = materialIng.getMatchingStacks()[0].getItem();
+
+        ShapedRecipeBuilder.shapedRecipe(result.get())
+                .patternLine("I")
+                .patternLine("S")
+                .key('I', materialIng)
+                .key('S', Items.STICK)
+                .addCriterion("has_"+material.getRegistryName().getPath(), InventoryChangeTrigger.Instance.forItems(material))
+                .build(consumer);
+    }
+
+    public void buildMaterialRecipeSet(String matName, Consumer<IFinishedRecipe> consumer, Supplier<Item> ingot, Supplier<Item> nugget, Supplier<Block> block) {
+        ShapelessRecipeBuilder.shapelessRecipe(block.get())
+                .addIngredient(block.get(), 9)
+                .addCriterion("has_"+matName+"_ingot", this.hasItem(ingot.get()))
+                .build(consumer);
+
+        ShapelessRecipeBuilder.shapelessRecipe(ingot.get(), 9)
+                .addIngredient(block.get())
+                .addCriterion("has_"+matName+"_ingot", this.hasItem(ingot.get()))
+                .setGroup(ingot.get().getRegistryName().getNamespace())
+                .build(consumer, id(matName+"_ingot_from_block"));
+
+        ShapelessRecipeBuilder.shapelessRecipe(nugget.get(), 9)
+                .addIngredient(ingot.get())
+                .addCriterion("has_"+matName+"_ingot", this.hasItem(ingot.get()))
+                .build(consumer);
+
+        ShapelessRecipeBuilder.shapelessRecipe(ingot.get())
+                .addIngredient(nugget.get(), 9)
+                .addCriterion("has_"+matName+"_ingot", this.hasItem(ingot.get()))
+                .setGroup(ingot.get().getRegistryName().getNamespace())
+                .build(consumer, id(matName+"_ingot_from_nugget"));
     }
 
     private static ResourceLocation id(String name) {
